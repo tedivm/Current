0bb379362e4fd65b6405255210475e50f69a70b0
Robert Hafner <tedivm@tedivm.com>
2014-07-02 02:22:58 -0700

    Created Manifest class
    This class takes a Manifest and organizes the resulting releases to
    identify which ones are most current.

---
+++ src/Current/Manifest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Current;
+
+use Current\Interfaces\Source;
+
+class Manifest
+{
+    protected static $sources = array('https://github.com/' => 'Github');
+    protected static $defaultSource = '';
+
+    protected $source;
+
+    protected $releases = array();
+    protected $versions = array();
+
+    protected $latests = array();
+
+    public static function buildFromUrl($url)
+    {
+        $className = static::$defaultSource;
+        foreach (self::$sources as $sourceUrl => $sourceClass) {
+            if (substr($url, 0, strlen($sourceUrl)) == $sourceUrl) {
+                $className = 'Current\\Sources\\' . $sourceClass;
+                break;
+            }
+        }
+
+        /** @var Source $source */
+        $source = new $className();
+        $source->initialize($url);
+
+        return new self($source);
+    }
+
+    public function __construct(Source $source)
+    {
+        $this->source = $source;
+
+        foreach ($source->getReleases() as $releaseConfig) {
+
+            $version = new Version($releaseConfig['version'], !$releaseConfig['stable'] ? true : null);
+            $this->releases[$version->getLongString()] = $releaseConfig;
+            $this->versions[] = $version;
+        }
+
+        usort($this->versions, 'Current\\Version::compare');
+
+        /** @var Version $versionObject */
+        foreach ($this->versions as $versionObject) {
+            $versionObject->getMajor();
+            $this->latests[$versionObject->getMajor()] = $versionObject;
+        }
+    }
+
+    public function getLatestVersion($macro = null)
+    {
+        if (isset($macro)) {
+            $macroGroup = $this->latests[$macro];
+        } else {
+            $macroGroup = end($this->latests);
+        }
+
+        return end($macroGroup);
+    }
+
+    public function getReleaseFromVersion(Version $version)
+    {
+        return $this->releases[$version->getLongString()];
+    }
+
+}
