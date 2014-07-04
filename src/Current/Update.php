f50d8414204fe94d39bdc195a1aac4ba161ba6df
Robert Hafner <tedivm@tedivm.com>
2014-07-04 01:40:05 -0700

    Added Update class

--- src/Current/Update.php
+++ src/Current/Update.php
@@ -6,11 +6,24 @@ use Current\Interfaces\Source;
 
 class Update
 {
+    /**
+     * @var Version
+     */
     protected $currentVersion;
+
+    /**
+     * @var Manifest
+     */
     protected $manifest;
+
+    protected $applicationPath;
 
     protected static $sources = array('https://github.com/' => 'Github');
     protected static $defaultSource = '';
+
+    const MAJOR = 1;
+    const MINOR = 2;
+    const PATCH = 4;
 
     public static function buildFromUrl($url)
     {
@@ -31,9 +44,11 @@ class Update
         return new self($manifest);
     }
 
-    public function __construct(Manifest $manifest)
+    public function __construct(Manifest $manifest, $applicationPath = null)
     {
         $this->manifest = $manifest;
+        $this->applicationPath = !is_null($applicationPath) ? $applicationPath : realpath($_SERVER['argv'][0]);
+
     }
 
     public function setCurrentVersion($version)
@@ -41,10 +56,36 @@ class Update
         if ($version instanceof Version) {
             $this->currentVersion = $version;
         } elseif (is_string($version)) {
-            $version = new Version($version);
-            $this->currentVersion = $version;
+            $this->currentVersion = new Version($version);;
         } else {
             throw new \RuntimeException('Invalid version.');
         }
     }
+
+    public function update($level = self::MAJOR)
+    {
+        if ($level | self::MAJOR) {
+            $updateVersion = $this->manifest->getLatestVersion(true);
+        } elseif ($level | self::MINOR) {
+            $updateVersion = $this->manifest->getLatestVersion(true, $this->currentVersion->getMajor());
+        } else {
+            $updateVersion = $this->manifest->getLatestVersion(true, $this->currentVersion->getMajor(), $this->currentVersion->getMinor());
+        }
+
+        if ($updateVersion === false) {
+            // There are no versions available with the requested limits.
+            return true;
+        }
+
+        if (isset($this->currentVersion) && Version::compare($this->currentVersion, $updateVersion)) {
+            // nothing to do, this is the current version
+            return true;
+        }
+
+        $release = $this->manifest->getReleaseFromVersion($updateVersion);
+        $tmp = $release->saveToTemp();
+
+        return rename($tmp, $this->applicationPath);
+    }
+
 }
